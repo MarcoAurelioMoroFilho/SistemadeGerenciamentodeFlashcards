ffb6dddecad597f2c2297b7904d0a22ab3edfda7
Marco Aurélio Moro Filho <moromarcolions@gmail.com>
2026-05-05 09:02:33 -0300

    Add function to list flashcards by deck ID

--- read/listarPorBaralho.js
+++ read/listarPorBaralho.js
@@ -1,3 +1,19 @@
 import baralho from "bank/bancoBaralhos.js"
 import flashcards from "bank/bancoFlashcards.js"
 
+function listarPorBaralho(idBaralho) {
+    const baralhoEncontrado = baralho.find((baralho) => baralho.id === idBaralho)
+
+    if (!baralhoEncontrado) {
+        return "Baralho não encontrado."
+    }
+
+    const flashcardsDoBaralho = flashcards.filter((flashcard) => flashcard.idBaralho === idBaralho)
+
+    if (flashcardsDoBaralho.length === 0) {
+        return "Nenhum flashcard encontrado para este baralho."
+    }
+
+    return flashcardsDoBaralho
+}
+export default listarPorBaralho
